b24c57d9b64b1ee8aac669fe334e6186d4ce0a4a
Doridian <git@doridian.net>
2024-11-17 22:18:55 -0800

    set

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-sudo docker build --target=builder -t pdns-builder . && docker run --rm -it pdns-builder
+#!/bin/bash
+set -euo pipefail
+
+sudo docker build --target=builder -t pdns-builder .
+sudo docker run --rm -it pdns-builder
