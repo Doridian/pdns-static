8b8b31020cb2e8403bb3f2f8756ccd15ed9d7660
Doridian <git@doridian.net>
2024-11-19 10:12:22 -0800

    Add pull

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+podman pull ghcr.io/doridian/pdns-static/pdns-static:latest
 podman build --target=builder -t pdns-builder .
 podman run --rm -it pdns-builder
 #podman build -t pdns-static .
